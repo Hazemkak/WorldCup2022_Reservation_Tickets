ce2749532d62d77e4c45d32876c3895e2877b9d2
Abdullah Adel <abdullahadel.aam@gmail.com>
2022-12-03 13:48:09 +0200

    Modify environment handling

--- server/main/settings.py
+++ server/main/settings.py
@@ -10,15 +10,13 @@ env = environ.Env()
 environ.Env.read_env()
 
 SECRET_KEY = env("SECRET_KEY")
-DEBUG = env("DEBUG")
+DEBUG = env("DEBUG") == "True"
 DB_NAME = env("DB_NAME")
 
 DB_USER = env("DB_USER")
 DB_PASSWORD = env("DB_PASSWORD")
 DB_HOST = env("DB_HOST")
 DB_PORT = env('DB_PORT')
-
-DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
