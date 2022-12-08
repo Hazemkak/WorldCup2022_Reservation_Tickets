e45c9de4ea329d6e3a1877a2e9cd1395a4d3f2aa
hazem kasim <hazem.kak@intellaworld.com>
2022-12-09 01:06:41 +0200

    Fix: fixing ts errors of config

--- client/src/shared/Layout/index.tsx
+++ client/src/shared/Layout/index.tsx
@@ -1,45 +1,46 @@
-import React from "react";
+import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { BasicProps } from "../../types";
 import Navbar from "../Navbar";
 
 function Copyright() {
-    return (
-        <Typography variant="body2" color="text.secondary" align="center">
-            {"Copyright © FIFA - CUFE Branch "}
-            {new Date().getFullYear()}
-            {"."}
-        </Typography>
-    );
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © FIFA - CUFE Branch "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
 }
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: "#550065",
-        },
+  palette: {
+    primary: {
+      main: "#550065",
     },
-    typography: {
-        fontFamily: "Poppins, sans-serif",
-    },
+  },
+  typography: {
+    fontFamily: "Poppins, sans-serif",
+  },
 });
-
-const Layout: React.FC<BasicProps> = ({ children }) => {
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Navbar />
-            <main>
-                <Box sx={{ m: 6, mt: 12 }}>{children}</Box>
-            </main>
-            <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
-                <Copyright />
-            </Box>
-        </ThemeProvider>
-    );
+interface LayoutProps {
+  children?: React.ReactNode;
+}
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Navbar />
+      <main>
+        <Box sx={{ m: 6, mt: 12 }}>{children}</Box>
+      </main>
+      <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
+        <Copyright />
+      </Box>
+    </ThemeProvider>
+  );
 };
 
 export default Layout;
--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,16 +1,12 @@
-export interface BasicProps {
-    children?: React.ReactNode;
-}
-
 export interface User {
-    id: number;
-    first_name: string;
-    last_name: string;
-    email: string;
-    username: string;
-    birthDate: string;
-    gender: string;
-    nationality: string;
-    role: string;
-    isVerified: boolean;
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  username: string;
+  birthDate: string;
+  gender: string;
+  nationality: string;
+  role: string;
+  isVerified: boolean;
 }
