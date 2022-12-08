6d35544931395588a63f5ac2c53b53d7ecdc76bd
hazem kasim <hazem.kak@intellaworld.com>
2022-12-09 01:18:50 +0200

    Feat: adding all the types of data

---
+++ client/src/pages/reservations/ReservationsList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ReservationsList() {
+  return (
+    <div>ReservationsList</div>
+  )
+}
+
+export default ReservationsList
--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,3 +1,4 @@
+type ROLES = "0" | "1" | "2";
 export interface User {
   id: number;
   first_name: string;
@@ -9,4 +10,43 @@ export interface User {
   nationality: string;
   role: string;
   isVerified: boolean;
+}
+
+export interface Stadium {
+  id: number;
+  name: string;
+  city: string;
+  country: string;
+  rows: number;
+  seatsPerRow: number;
+}
+
+export interface Team {
+  id: number;
+  name: string;
+  img_url: string;
+}
+
+export interface Referee {
+  id: number;
+  firstName: string;
+  lastName: string;
+  role: ROLES;
+}
+
+export interface Match {
+  id: number;
+  stadium: Stadium;
+  match_date: string;
+  match_time: string;
+  teams: Array<Team>;
+  referees: Array<Referee>;
+}
+
+export interface Reservation {
+  id: number;
+  user: User;
+  match: Match;
+  reservationDate: string;
+  seatId: number;
 }
